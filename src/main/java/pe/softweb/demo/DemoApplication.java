32342ac89a5a1772eacc106c154ee9b86275f3c9
Pepe Valdivia <pepe.valdivia.caballero@gmail.com>
2024-02-17 16:53:14 -0500

    hola mudno

--- src/main/java/pe/softweb/demo/DemoApplication.java
+++ src/main/java/pe/softweb/demo/DemoApplication.java
@@ -2,6 +2,9 @@ package pe.softweb.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +13,14 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+}
+
+@RestController
+@RequestMapping("/api")
+class HelloWorldController {
+
+    @GetMapping("/hola")
+    public String helloWorld() {
+        return "Hola Mundo";
+    }
 }
